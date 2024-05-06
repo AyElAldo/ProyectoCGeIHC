03c48a330e372b3c62ae8cee21d007be8d45f444
Axel N <AxelNunezArzola11@users.noreply.github.com>
2024-05-06 12:29:15 -0600

    AGREGUE EDIFICIOS CON VENTANAS

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -98,6 +98,15 @@ Model mesasillaplastico;
 ///estatua dorada spider man
 Model estatuaspidermandorada;
 
+/// Bicicleta roja
+Model bicicletaroja;
+///BALON BASQUET
+Model pelotabasquet;
+////tienda comic ventana con transparencia
+Model tiendacomic;
+///tienda de dulce transparencia
+Model candystore;
+Model tiendavideojuegos;
 
 Skybox skybox;
 
@@ -306,8 +315,33 @@ int main()
 	///estatua dorada spiderman
 	estatuaspidermandorada = Model();
 	estatuaspidermandorada.LoadModel("Models/estatuaspiderman.obj");
+	
+///fuente agua
+	//fuenteagua=Model();
+	//fuenteagua.LoadModel("Models/fuenteagua.obj");
+	////bicicleta roja
+	bicicletaroja=Model();
+	bicicletaroja.LoadModel("Models/bicicletaroja.obj");
+	///pelota basquet
+	pelotabasquet=Model();
+	pelotabasquet.LoadModel("Models/pelotbasquet.obj");
 
+/************************** ventana transparente*****************************/
+
+	///tienda comic ventana transparente
+
+	tiendacomic=Model();
+	tiendacomic.LoadModel("Models/tiendacomic.obj");
+
+	candystore=Model();
+	candystore.LoadModel("Models/candystore.obj");
+	tiendavideojuegos=Model();
+	tiendavideojuegos.LoadModel("Models/tiendavideojuegos.obj");
+
+	
+	
 	/************************** Hora de aventura *****************************/
+
 	// Castillo del rey helado
 	castillo_rey_helado = Model();
 	castillo_rey_helado.LoadModel("Models/king_castle.obj");
@@ -320,7 +354,7 @@ int main()
 
 	// Entrada unicornio
 	Rainicorn = Model();
-	Rainicorn.LoadModel("Models/Rainicorn_solo.obj");
+	Rainicorn.LoadModel("Models/rainicorn_solo.obj");
 
 	// Fuente del centro de la alameda
 	Fuente01 = Model();
@@ -695,7 +729,47 @@ int main()
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-3.0f, 1.0f, 8.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		estatuaspidermandorada.RenderModel();	
+		estatuaspidermandorada.RenderModel();
+		///bicicleta roja
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(5.0f, 1.2f, 8.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		bicicletaroja.RenderModel();
+		///pelotabasquet
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(5.0f, 1.4f, 4.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		pelotabasquet.RenderModel();
+
+
+		////tienda comic con ventana transparente
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-50.0f, 1.0f, -15.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		tiendacomic.RenderModel();
+		///tienda de dulce
+
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-50.0f, 1.0f, -30.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		candystore.RenderModel();
+		///tienda videojuegos
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-50.0f, 1.0f, 10.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		tiendavideojuegos.RenderModel();
+
+		//glEnable(GL_BLEND);
+		//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+
+
 
 
 		/************ Hora de Aventura **************/
